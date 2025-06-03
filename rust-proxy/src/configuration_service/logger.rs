7bc633fe02000eb4c92e2f2476abe1611b86df22
shikai liu <lsk569937453@gmail.com>
2025-06-03 16:30:27 +0800

    Update the crates.

--- rust-proxy/src/configuration_service/logger.rs
+++ rust-proxy/src/configuration_service/logger.rs
@@ -29,7 +29,7 @@ pub fn setup_logger() -> Result<Handle<Targets, Registry>, AppError> {
 
 pub fn setup_logger() -> Result<Handle<Targets, Registry>, AppError> {
     let rolling_file_builder = RollingFileAppender::builder()
-        .rotation(rolling::Rotation::MINUTELY)
+        .rotation(rolling::Rotation::HOURLY)
         .filename_prefix("spire")
         .filename_suffix("log")
         .max_log_files(10)
