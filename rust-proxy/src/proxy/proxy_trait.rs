94bda2dd213b519facf6f279264a5f8296a0f08d
lsk569937453 <lsk569937453@gmail.com>
2025-06-13 11:06:57 +0800

    Dev (#65)
    * Update the error message.
    
    * Update the error message.
    
    * Update the error message.
    
    * fix the bug of poll route
    
    * fix the bug of header based route
    
    * fix the bug of weighy based route
    
    * fix the bug of weighy based route
    
    * fix the bug of weighy based route
    
    * fix the bug of weighy based route
    
    * Add unit test.
    
    * Add unit test.
    
    * Add unit test.
    
    * Add unit test.
    
    * Add unit test.
    
    * Add unit test.
    
    * Add unit test.
    
    * First commit.
    
    * Delete the older log.
    
    * Update the middleware and the cors config.
    
    * Update the middleware and the cors config.
    
    * fix the bug of weighy based route
    
    * Remove acme-lib.
    
    * fix the bug of weighy based route
    
    * fix the bug of weighy based route
    
    * Remove acme-lib.
    
    * Fix the bug of unwrap.
    
    * Fix the bug of unwrap.
    
    * fix the bug of weighy based route
    
    * Fix the bug of unwrap.
    
    * Update the log.
    
    * Update the crates.
    
    * Update the crates.
    
    * fix the bug of weighy based route
    
    * fix the bug of weighy based route
    
    * Add the simple config.
    
    * Fix the bug of http headers.
    
    * Update the simple config.
    
    * Update the simple config.
    
    * Update the simple config.
    
    * Update the simple config.
    
    * Update the simple config.
    
    * Improve the performance.
    
    * Upgrade the crates.
    
    * Add desrialization for the route_cluster.
    
    * Add static config.
    
    * Add router.
    
    * Optimize the name .
    
    * Optimize the name .
    
    * add the middleware .
    
    * add the middleware of headers.
    
    * Change the log level.
    
    * Change the log level.
    
    * Add the unit test.
    
    * fix the bug of weighy based route
    
    * Update the README.md.
    
    * Update the README.md.
    
    * Update the README.md.
    
    * Add accesslog for rest api.
    
    * Update the crates.
    
    * Update the crates.
    
    * add the deploy bat.
    
    * add the deploy bat.
    
    * add the deploy bat.
    
    * add the deploy bat.
    
    * add the deploy bat.
    
    * add the deploy bat.
    
    * fix the bug of weighy based route
    
    * add the unit test.
    
    * add the unit test.
    
    * fix the bug of weighy based route
    
    * add the unit test.
    
    * add the unit test.
    
    * add the unit test.
    
    * add the unit test.
    
    * add the unit test.
    
    * add the unit test.
    
    * add the unit test.
    
    * add the unit test.
    
    * add the unit test.
    
    * add the unit test.
    
    * add the unit test.
    
    * add the unit test.
    
    * add the unit test.
    
    * add the unit test.
    
    * Update the version of spire.
    
    * Update the version of spire.
    
    * Update the github flows.
    
    * Update the github flows.
    
    * Update the version of spire.
    
    * Update the version of spire.
    
    * Update log.
    
    * tiny change

--- rust-proxy/src/proxy/proxy_trait.rs
+++ rust-proxy/src/proxy/proxy_trait.rs
@@ -232,6 +232,198 @@ impl ChainTrait for CommonCheckRequest {
         )?)
     }
 }
+#[automock]
+pub trait ChainTrait {
+    #[allow(clippy::too_many_arguments)]
+    async fn get_destination(
+        &self,
+        shared_config: SharedConfig,
+        port: i32,
+        mapping_key: String,
+        headers: &HeaderMap,
+        uri: Uri,
+        peer_addr: SocketAddr,
+        spire_context: &mut SpireContext,
+    ) -> Result<Option<HandlingResult>, AppError>;
+    async fn handle_before_response(
+        &self,
+        middlewares: Vec<MiddleWares>,
+        req_path: &str,
+        response: &mut Response<BoxBody<Bytes, AppError>>,
+    ) -> Result<(), AppError>;
+    fn handle_preflight(
+        &self,
+        cors_config: CorsConfig,
+        origin: &str,
+    ) -> Result<Response<BoxBody<Bytes, AppError>>, AppError>;
+    async fn handle_before_request(
+        &self,
+        middlewares: Vec<MiddleWares>,
+        peer_addr: SocketAddr,
+        req: &mut Request<BoxBody<Bytes, AppError>>,
+    ) -> Result<(), AppError>;
+}
+pub struct CommonCheckRequest;
+
+#[derive(Debug, Clone)]
+pub struct HandlingResult {
+    pub request_path: String,
+    pub router_destination: RouterDestination,
+}
+#[derive(Debug, Clone, PartialEq, Eq)]
+
+pub enum RouterDestination {
+    Http(BaseRoute),
+    File(StaticFileRoute),
+}
+impl RouterDestination {
+    pub fn get_endpoint(&self) -> String {
+        match self {
+            RouterDestination::Http(base_route) => base_route.endpoint.clone(),
+            RouterDestination::File(static_file_route) => static_file_route.doc_root.clone(),
+        }
+    }
+
+    pub fn is_file(&self) -> bool {
+        match self {
+            RouterDestination::Http(_) => false,
+            RouterDestination::File(_) => true,
+        }
+    }
+}
+impl ChainTrait for CommonCheckRequest {
+    async fn handle_before_response(
+        &self,
+        middlewares: Vec<MiddleWares>,
+        req_path: &str,
+
+        response: &mut Response<BoxBody<Bytes, AppError>>,
+    ) -> Result<(), AppError> {
+        for item in middlewares.iter() {
+            item.handle_before_response(req_path, response)?;
+        }
+
+        Ok(())
+    }
+    async fn handle_before_request(
+        &self,
+        middlewares: Vec<MiddleWares>,
+
+        peer_addr: SocketAddr,
+        req: &mut Request<BoxBody<Bytes, AppError>>,
+    ) -> Result<(), AppError> {
+        for item in middlewares.iter() {
+            item.handle_before_request(peer_addr, req)?;
+        }
+        Ok(())
+    }
+    async fn get_destination(
+        &self,
+        shared_config: SharedConfig,
+        port: i32,
+        _mapping_key: String,
+        headers: &HeaderMap,
+        uri: Uri,
+        peer_addr: SocketAddr,
+        spire_context: &mut SpireContext,
+    ) -> Result<Option<HandlingResult>, AppError> {
+        let backend_path = uri
+            .path_and_query()
+            .ok_or(AppError::from("Path is empty"))?
+            .as_str();
+        let mut app_config = shared_config.shared_data.lock()?;
+        let api_service = app_config
+            .api_service_config
+            .get_mut(&port)
+            .ok_or(AppError::from(
+                "Can not find config by port from app config.",
+            ))?;
+
+        for item in api_service.route_configs.iter_mut() {
+            let match_result = item.is_matched(backend_path, Some(headers))?;
+            if match_result.is_none() {
+                continue;
+            }
+            let is_allowed = item.is_allowed(&peer_addr, Some(headers))?;
+            if !is_allowed {
+                return Ok(None);
+            }
+            let router_destination = item.router.get_route(headers)?;
+            let rest_path = match_result.ok_or("match_result is none")?;
+
+            match router_destination {
+                RouterDestination::File(file_route) => {
+                    let path = Path::new(&file_route.doc_root);
+                    let request_path = path.join(rest_path);
+                    spire_context.middlewares = item.middlewares.clone();
+                    return Ok(Some(HandlingResult {
+                        request_path: String::from(request_path.to_str().unwrap_or_default()),
+                        router_destination: RouterDestination::File(file_route),
+                    }));
+                }
+                RouterDestination::Http(base_route) => {
+                    let request_path = [base_route.endpoint.as_str(), rest_path.as_str()].join("/");
+                    spire_context.middlewares = item.middlewares.clone();
+                    return Ok(Some(HandlingResult {
+                        request_path,
+                        router_destination: RouterDestination::Http(base_route.clone()),
+                    }));
+                }
+            }
+        }
+        Ok(None)
+    }
+    fn handle_preflight(
+        &self,
+        cors_config: CorsConfig,
+        origin: &str,
+    ) -> Result<Response<BoxBody<Bytes, AppError>>, AppError> {
+        if cors_config.validate_origin("")? {
+            return Ok(Response::builder().status(StatusCode::FORBIDDEN).body(
+                Full::new(Bytes::from("".to_string()))
+                    .map_err(AppError::from)
+                    .boxed(),
+            )?);
+        }
+        let methods_header = cors_config
+            .allowed_methods
+            .iter()
+            .map(|m| m.as_str()) // Convert serde_json::Error to AppError
+            .collect::<Vec<&str>>()
+            .join(", ");
+        let headers_header = cors_config
+            .allowed_headers
+            .iter()
+            .map(|m| m.to_string()) // Convert serde_json::Error to AppError
+            .collect::<Vec<String>>()
+            .join(", ");
+
+        let mut builder = Response::builder()
+            .status(StatusCode::NO_CONTENT)
+            .header(header::ACCESS_CONTROL_ALLOW_METHODS, methods_header)
+            .header(header::ACCESS_CONTROL_ALLOW_HEADERS, headers_header)
+            .header(header::ACCESS_CONTROL_ALLOW_ORIGIN, origin)
+            .header(header::VARY, "Origin");
+
+        if let Some(s) = cors_config.allow_credentials {
+            if s {
+                builder = builder.header(
+                    header::ACCESS_CONTROL_ALLOW_CREDENTIALS,
+                    HeaderValue::from_static("true"),
+                );
+            }
+        }
+        if let Some(s) = cors_config.max_age {
+            builder = builder.header(header::ACCESS_CONTROL_MAX_AGE, s.to_string());
+        }
+
+        Ok(builder.body(
+            Full::new(Bytes::from("".to_string()))
+                .map_err(AppError::from)
+                .boxed(),
+        )?)
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
